3256ddfd4bcf8964a1610b0d6927596bca50599d
nardo-leo <jora.karuzo@gmail.com>
2020-04-27 15:40:47 +0300

    Fist worked version

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
-# scripts.ps.genGif
+# Project Title
 
-Plugin generates Gif from pack of banners
+Photoshop plugin, that changes current frame from pack of banners and auto saves file as a Gif.
 
-Сценарий работы с плагином:
-- [] Алгоритм _saveFromDatasetToJpg cгенерирует 1ые кадры баннеры в  отдельную директорию;
-- [] Открыть макет для Gif-баннера в Ps;
-- [] Запустить скрипт;
-- [] Выбрать директорию, из которой будут забираться баннеры для 1-го кадра;
-- [] Указать директорию, в которую будут собираться gif.
+### Installing
+
+Move .jsx file to Photoshop's script directory.
+
+Example for macOS: /Applications/Adobe Photoshop CC 2020/Presets/Scripts
+
+Restart Photoshop then go to File >> Script  to access.
+
+## Getting Started
+
+1. Generate pack of banners (____saveFromDatasetToJpg plugin is highly recommended);
+2. Build a PSD-template of Gif;
+3. Choose layer, you need to change in template and convert it to Smart Layer;
+4. Make shure, that layer size and size of earlier generated banners are equal;
+5. Start script and follow instructions.
+
+## Authors
+
+* Leo Nardo
+
+See also the list of [contributors](http://192.168.88.240:3000/leonardo/scripts.ps.genGif) who participated in this project.
--- _genGif.jsx
+++ _genGif.jsx
@@ -1,60 +1,66 @@
-// choose dirs
-var srcDir = Folder.selectDialog("Choose Source Folder", "");
-var outputDir = Folder.selectDialog("Choose Output Folder", "");
+function main() {
+    // choose dirs
+    var srcDir = Folder.selectDialog("Choose Source Folder", "");
+    var outputDir = Folder.selectDialog("Choose Output Folder", "");
 
-var myDocument = app.activeDocument;
-var smartLayer = myDocument.activeLayer;
+    var myDocument = app.activeDocument;
+    var smartLayer = myDocument.activeLayer;
 
-// get all files in srcDir
-var fileList = srcDir.getFiles();
+    // get all files in srcDir
+    var fileList = srcDir.getFiles();
 
-// replace smartLayer content
-function replaceContents(newFile, sLayer) {
-    app.activeDocument.activeLayer = sLayer;
-    var idplacedLayerReplaceContents = stringIDToTypeID("placedLayerReplaceContents");
-    var desc3 = new ActionDescriptor();
-    var idnull = charIDToTypeID("null");
-    desc3.putPath(idnull, new File(newFile));
-    var idPgNm = charIDToTypeID("PgNm");
-    desc3.putInteger(idPgNm, 1);
-    executeAction(idplacedLayerReplaceContents, desc3, DialogModes.NO);
-    return app.activeDocument.activeLayer
-};
+    // replace smartLayer content
+    function replaceContents(newFile, sLayer) {
+        app.activeDocument.activeLayer = sLayer;
+        var idplacedLayerReplaceContents = stringIDToTypeID("placedLayerReplaceContents");
+        var desc3 = new ActionDescriptor();
+        var idnull = charIDToTypeID("null");
+        desc3.putPath(idnull, new File(newFile));
+        var idPgNm = charIDToTypeID("PgNm");
+        desc3.putInteger(idPgNm, 1);
+        executeAction(idplacedLayerReplaceContents, desc3, DialogModes.NO);
+        return app.activeDocument.activeLayer
+    };
 
-// save for web
-function saveForWeb(saveFile) {
-    var sfwOptions = new ExportOptionsSaveForWeb();
-    sfwOptions.format = SaveDocumentType.COMPUSERVEGIF;
-    sfwOptions.includeProfile = false;
-    sfwOptions.interlaced = 1;
-    sfwOptions.optimized = true;
-    sfwOptions.transparency = 1;
-    sfwOptions.ColorReductionType = ColorReductionType.SELECTIVE;
-    sfwOptions.dither = Dither.NONE;
-    sfwOptions.ditherAmount = 80;
-    sfwOptions.webSnap = 0;
-    sfwOptions.colors = 128;
-    activeDocument.exportDocument(saveFile, ExportType.SAVEFORWEB, sfwOptions);
-}
+    // save for web
+    function saveForWeb(saveFile) {
+        var sfwOptions = new ExportOptionsSaveForWeb();
+        sfwOptions.format = SaveDocumentType.COMPUSERVEGIF;
+        sfwOptions.includeProfile = false;
+        sfwOptions.interlaced = 1;
+        sfwOptions.optimized = true;
+        sfwOptions.transparency = 1;
+        sfwOptions.ColorReductionType = ColorReductionType.SELECTIVE;
+        sfwOptions.dither = Dither.NONE;
+        sfwOptions.ditherAmount = 80;
+        sfwOptions.quality = 0;
+        sfwOptions.webSnap = 0;
+        sfwOptions.colors = 128;
+        activeDocument.exportDocument(saveFile, ExportType.SAVEFORWEB, sfwOptions);
+    }
 
-// open each file as first layer and save as a gif
-for (var i = 0; i < fileList.length; i++){
+    // open each file as first layer and save as a gif
+    for (var i = 0; i < fileList.length; i++){
 
-    // open only files
-    if (fileList[i] instanceof File) {
+        // open only files
+        if (fileList[i] instanceof File) {
 
-        // check if layer is SmartObject
-        if (smartLayer.kind == "LayerKind.SMARTOBJECT") {
+            // check if layer is SmartObject
+            if (smartLayer.kind == "LayerKind.SMARTOBJECT") {
 
-            // change Smartobject layer to fileList[i]
-            smartLayer = replaceContents(fileList[i], smartLayer);
+                // change Smartobject layer to fileList[i]
+                smartLayer = replaceContents(fileList[i], smartLayer);
 
-            // save as a Gif
-            var name = decodeURI(myDocument.name).replace(/\.[^\.]+$/, '');
-            var saveFile = new File(outputDir + "/" + name + "-" + i + ".gif");
-            saveForWeb(saveFile);
-        } else {
-            alert("Selected Layer is not a smart object");
+                // save as a Gif
+                var name = decodeURI(myDocument.name).replace(/\.[^\.]+$/, '');
+                var saveFile = new File(outputDir + "/" + name + "-" + (i + 1) + ".gif");
+                saveForWeb(saveFile);
+            } else {
+                alert("Selected Layer is not a smart object");
+                return;
+            }
         }
     }
 }
+
+main();
